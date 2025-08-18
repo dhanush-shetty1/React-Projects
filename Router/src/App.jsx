fe0bbd4848abe97b7f57fe01545dac8a43a39650
dhanush-shetty1 <dhanush.shetty@somaiya.edu>
2025-08-18 18:34:34 +0530

    Routing

---
+++ Router/src/App.jsx
@@ -0,0 +1,22 @@
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import { Home } from "./Pages/Home"
+import { Menu } from "./Pages/Menu"
+import { Contact } from "./Pages/Contact"
+import { Navbar } from "./Navbar"
+function App() {
+  return(
+    <div className="App">
+      <Router>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/menu" element={<Menu/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="*" element={<h1>Url Toh Dhang se Daal Page</h1>}/>
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
