d3db67b4fe539003a3a00ad36af5d2a14ffec82b
dhanush-shetty1 <dhanush.shetty@somaiya.edu>
2025-09-06 13:16:15 +0530

    Forms

---
+++ Forms/src/Form.jsx
@@ -0,0 +1,18 @@
+import {useForm} from "react-hook-form"
+import * as yup from 'yup'
+export const Form =()=>{
+    const {register,handleSubmit}=useForm();
+    const onSubmit=(data)=>{
+        console.log(data);
+    }
+    return(
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="Username"{...register("fullName")} /><br/>
+            <input type="text" placeholder="Email" {...register("email")} /><br/>
+            <input type="number" placeholder="Age" {...register("Age")} /><br/>
+            <input type="password" placeholder="Password"{...register("Password")} /><br/>
+            <input type="password" placeholder="Confirm Password"{...register("confirmPassword")} /><br/>
+            <input type="submit" />
+        </form>
+    )
+}
